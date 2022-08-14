82c4a4c1b3bcf41c5dff4ca8adf7ac23e18ba85a
amirkalmoni <amirkalmoni@gmail.com>
2022-08-14 19:45:32 -0400

    minor features added

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-## Mirotron is a script that will sync a local directory to an s3 bucket
+## Mirotron
+This is a script that will sync a local directory to an s3 bucket and optionally configure Route 53 and cloudfront
+
+## Features
+
+Mirotron currently has the following features:
+
+-list all buckets
+-list all objects in a specified bucket
+
---
+++ mirotron/mirotron.py
@@ -0,0 +1,27 @@
+import boto3
+import click
+
+session = boto3.Session(profile_name='amirkalmoni')
+s3 = session.resource('s3')
+
+@click.group()
+def cli():
+	"Mirotron deploys websites to AWS"
+	pass
+
+@cli.command('list-buckets')
+def list_buckets():
+	"List all s3 buckets"
+	for bucket in s3.buckets.all():
+		print(bucket)
+
+
+@cli.command('list-bucket-object')
+@click.argument('bucket')
+def list_bucket_object(bucket):
+	"Lists all objects in the specified bucket"
+	for object in s3.Bucket(bucket).objects.all():
+		print(object)
+
+if __name__ =='__main__':
+	cli()
